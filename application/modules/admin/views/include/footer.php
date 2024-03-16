d865fd1c2a1f3df14475ec915fdbe986319d9bfb
dimubro <dimuth939@gmail.com>
2024-03-16 19:51:17 +0530

    Update footer.php

--- application/modules/admin/views/include/footer.php
+++ application/modules/admin/views/include/footer.php
@@ -1,7 +1,7 @@
 
     <!-- Bootstrap JS (optional, but may be required for certain components) -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.7/dist/umd/popper.min.js"></script> -->
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js" integrity="sha512-2rNj2KJ+D8s1ceNasTIex6z4HWyOnEYLVC3FigGOmyQCZc2eBXKgOxQmo3oKLHyfcj53uz4QMsRCWNbLd32Q1g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
