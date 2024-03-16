22f1a1de2d4b2311c8caf69221610b7e20c2187e
dimubro <163578777+dimubro@users.noreply.github.com>
2024-03-16 19:53:09 +0530

    Update footer.php

--- application/modules/admin/views/include/footer.php
+++ application/modules/admin/views/include/footer.php
@@ -1,18 +1,13 @@
 
     <!-- Bootstrap JS (optional, but may be required for certain components) -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.7/dist/umd/popper.min.js"></script> -->
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js" integrity="sha512-2rNj2KJ+D8s1ceNasTIex6z4HWyOnEYLVC3FigGOmyQCZc2eBXKgOxQmo3oKLHyfcj53uz4QMsRCWNbLd32Q1g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 
 
-        <script src="<?= base_url() ?>assets/plugins/global/jquery-ui/jquery.ui.widget.min.js"></script>
-        <script src="<?= base_url() ?>assets/plugins/custom/jQuery-File-Upload/js/jquery.fileupload.js"></script>
-        <script src="<?= base_url() ?>assets/plugins/custom/jQuery-File-Upload/js/jquery.iframe-transport.js"></script>
-        <script src="<?= base_url() ?>assets/plugins/custom/jQuery-File-Upload/js/extras/load-image.min.js"></script>
-        <script src="<?= base_url() ?>assets/plugins/custom/jQuery-File-Upload/js/jquery.fileupload-process.js"></script>
-        <script src="<?= base_url() ?>assets/plugins/custom/jQuery-File-Upload/js/jquery.fileupload-image.js"></script>
+        
 
 
     <!-- Chart.js -->
